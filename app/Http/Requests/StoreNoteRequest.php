14f1faaa0467e57966aca2c5da240cc618ada36b
Yuta <ezochi66@gmail.com>
2023-10-02 07:09:39 +0900

    追加機能の実装

--- app/Http/Requests/StoreNoteRequest.php
+++ app/Http/Requests/StoreNoteRequest.php
@@ -11,7 +11,7 @@ class StoreNoteRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class StoreNoteRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'url' => ['required'],
+            'title' => ['required', 'max:100'],
+            'note' => [],
         ];
     }
 }
