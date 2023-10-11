1cd4c3e16b4f6bd37e7a07e116cd4681fc861b9c
Yuta <ezochi66@gmail.com>
2023-10-12 08:20:52 +0900

    ログイン認証解除

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,8 @@ Route::get('/dashboard', function () {
 Route::resource('notes', NoteController::class)
     ->middleware(['auth', 'verified']);
 
+Route::resource('notes', NoteController::class);
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
